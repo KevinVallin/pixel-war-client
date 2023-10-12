28ce1850283c69a3a8b81018f013d06523b7ccda
Kévin Vallin <kevin.vallin@ynov.com>
2023-10-12 20:15:35 +0200

    Add Client View

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,69 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <h1>Pixel War</h1>
+    <form @submit.prevent="submitPixel">
+      <label for="x">X Coordinate (0-999):</label>
+      <input type="number" id="x" v-model="x" min="0" max="999" required>
+      <label for="y">Y Coordinate (0-699):</label>
+      <input type="number" id="y" v-model="y" min="0" max="699" required>
+      <div :style="{ backgroundColor: selectedColor }" class="color-preview"></div>
+      <label for="color">Color (e.g., #FF5733):</label>
+      <input type="text" id="color" v-model="color" required>
+      <button type="submit">Change Color</button>
+    </form>
+    <canvas ref="pixelCanvas" width="1000" height="700" style="border: 1px solid black;"></canvas>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      grid: [],
+    };
+  },
+  mounted() {
+
+    this.loadGridData();
+    const socket = this.$socket;
+    socket.on('updatePixel', (data) => {
+      this.updatePixel(data.x, data.y, data.color);
+    });
+  },
+
+  methods: {
+    loadGridData() {
+    },
+    changeColor(x, y) {
+      const color = 'rgb(255, 0, 0)'/* Get the selected color */;
+      this.$socket.emit('changePixelColor', { x, y, color });
+    },
+    updatePixel(x, y, color) {
+      const row = this.grid.find((r) => r[0].y === y);
+      if (row) {
+        const pixel = row.find((p) => p.x === x);
+        if (pixel) {
+          pixel.color = color;
+        }
+      }
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.pixel-grid {
+  display: flex;
+  flex-direction: column;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.grid-row {
+  display: flex;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.pixel {
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueSocketIO from 'vue-3-socket.io';
+import SocketIO from 'socket.io-client'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const socketConnection = SocketIO('http://localhost:3000');
+
+console.log({app})
+app.use(new VueSocketIO({
+  debug: true,
+  connection: socketConnection,
+ /* vuex: {
+      store,
+      actionPrefix: 'SOCKET_',
+      mutationPrefix: 'SOCKET_'
+  },*/
+}))
+
+app.mount('#app')
+
+
+
